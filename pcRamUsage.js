80cad890f6c83e7c0f5b25581dc9179e976c39ea
Carlos Vinícius <carlos.viniciusufrpe@gmail.com>
2021-10-04 19:02:57 -0300

    adicionada função setInterval e funcionalidade de memória ram utilizada

--- pcRamUsage.js
+++ pcRamUsage.js
@@ -1,14 +1,19 @@
 const os = require('os');
 
-const { arch,platform,totalmem, freemem } = os;
-const tRam = totalmem()/1024/1024;
-const fRam = freemem()/ 1024/1024;
-
-const stats = {
-    OS: platform(),
-    Arch: arch(),
-    TotalRam: parseInt(tRam),
-    FreeRam: parseInt(fRam),
-};
-
-console.log(stats)
+setInterval(() =>{
+    const { arch,platform,totalmem, freemem } = os;
+    const tRam = totalmem()/1024/1024;
+    const fRam = freemem()/ 1024/1024;
+    const usage = ((tRam-fRam)/tRam ) * 100 ;
+    
+    const stats = {
+        OS: platform(),
+        Arch: arch(),
+        TotalRam: `${parseInt(tRam)} MB`,
+        FreeRam: `${parseInt(fRam)} MB`,
+        Usage: `${usage.toFixed(2)}%`
+    };
+    
+    console.clear();
+    console.table(stats)
+}, 1000);
